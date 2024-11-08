524b1e7354d247a3f110a72bb31900b8798bb831
Logic <38597904+LogicFan@users.noreply.github.com>
2024-11-08 03:41:40 -0500

    add test

--- src/database/user.rs
+++ src/database/user.rs
@@ -1,6 +1,5 @@
 use super::DATABASE;
 use crate::error::ServerError;
-use chrono::{DateTime, Duration, Utc};
 use core::str;
 use rusqlite::{Connection, Row};
 use sea_query::{enum_def, Expr, IdenStatic, Query, SqliteQueryBuilder};
@@ -17,9 +16,9 @@ pub struct User {
     pub username: String,
     pub password: Vec<u8>,
     #[serde(default)]
-    pub login_at: Option<DateTime<Utc>>,
+    pub login_at: (),
     #[serde(default)]
-    pub attempts: u64,
+    pub attempts: (),
 }
 
 impl PartialEq for User {
@@ -38,9 +37,24 @@ impl TryFrom<&Row<'_>> for User {
             id: value.get(UserIden::Id.as_str())?,
             username: value.get(UserIden::Username.as_str())?,
             password: value.get(UserIden::Password.as_str())?,
-            login_at: value.get(UserIden::LoginAt.as_str())?,
-            attempts: value.get(UserIden::Attempts.as_str())?,
+            login_at: (),
+            attempts: (),
         })
+    }
+}
+
+impl User {
+    pub fn new(
+        username: impl Into<String>,
+        password: impl Into<Vec<u8>>,
+    ) -> Self {
+        Self {
+            id: Uuid::nil(),
+            username: username.into(),
+            password: password.into(),
+            login_at: (),
+            attempts: (),
+        }
     }
 }
 
@@ -162,23 +176,50 @@ impl User {
         Ok(())
     }
 
-    pub fn attempts(&self, ttl: Duration) -> u64 {
-        if let Some(login_at) = self.login_at {
-            if login_at + ttl > Utc::now() {
-                return self.attempts;
-            }
-        }
-        return 0;
+    pub fn attempts(&self) -> Result<u64, ServerError> {
+        let (query, values) = Query::select()
+            .expr_as(
+                // if login_at is null, this condition will also fail
+                Expr::case(
+                    Expr::col(UserIden::LoginAt)
+                        .gt(Expr::cust("DATETIME('NOW', '-1 minutes')")),
+                    Expr::col(UserIden::Attempts),
+                )
+                .finally(0),
+                UserIden::Attempts,
+            )
+            .from(UserIden::Table)
+            .and_where(Expr::col(UserIden::Id).eq(self.id))
+            .build_rusqlite(SqliteQueryBuilder);
+
+        let connection = Connection::open(DATABASE)?;
+        let mut statement = connection.prepare(&query)?;
+        let record: Result<u64, rusqlite::Error> = statement
+            .query_and_then(&*values.as_params(), |row| {
+                row.get(UserIden::Attempts.as_str())
+            })?
+            .next()
+            .unwrap_or(Ok(0));
+
+        Ok(record?)
     }
 
-    pub fn add_attempt(&self, ttl: Duration) -> Result<(), ServerError> {
-        let attempts = self.attempts(ttl) + 1;
-
+    pub fn add_attempt(&self) -> Result<(), ServerError> {
         let (query, values) = Query::update()
             .table(UserIden::Table)
             .values([
                 (UserIden::LoginAt, Expr::cust("DATETIME('NOW')")),
-                (UserIden::Attempts, attempts.into()),
+                (
+                    UserIden::Attempts,
+                    // if login_at is null, this condition will also fail
+                    Expr::case(
+                        Expr::col(UserIden::LoginAt)
+                            .gt(Expr::cust("DATETIME('NOW', '-1 minutes')")),
+                        Expr::col(UserIden::Attempts).add(1),
+                    )
+                    .finally(1)
+                    .into(),
+                ),
             ])
             .and_where(Expr::col(UserIden::Id).eq(self.id))
             .build_rusqlite(SqliteQueryBuilder);
@@ -186,7 +227,7 @@ impl User {
         let connection = Connection::open(DATABASE)?;
         connection.execute(&query, &*values.as_params())?;
 
-        todo!()
+        Ok(())
     }
 }
 
@@ -195,38 +236,37 @@ mod tests {
     use super::*;
     use crate::database;
     use sha2::{Digest, Sha256};
+    use std::thread;
+    use std::time::Duration;
 
     #[test]
     fn test_insert_and_select() {
         database::init().expect("database initialization fail");
 
         let username = String::from("test_insert_and_select");
-        let u0 = User {
-            id: Uuid::nil(),
-            username: username.clone(),
-            password: Sha256::digest("test_password_1").to_vec(),
-            login_at: None,
-            attempts: 0,
-        };
-        let id = u0.insert().expect("insert panic");
-        assert_ne!(Uuid::nil(), id);
+        let mut u0 = User::new(
+            username.clone(),
+            Sha256::digest("test_password_1").to_vec(),
+        );
+        u0.id = u0.insert().expect("insert panic");
+        assert_ne!(Uuid::nil(), u0.id);
 
         let u1 = User::by_username(username.clone())
             .expect("select by username panic")
             .expect("user does not exist");
-        assert_eq!(id, u1.id);
+        assert_eq!(u0.id, u1.id);
         assert_eq!(u0.username, u1.username);
         assert_eq!(u0.password, u1.password);
 
-        let u2 = User::by_id(id)
+        let u2 = User::by_id(u0.id)
             .expect("select by id panic")
             .expect("user does not exist");
-        assert_eq!(id, u2.id);
+        assert_eq!(u0.id, u2.id);
         assert_eq!(u0.username, u2.username);
         assert_eq!(u0.password, u2.password);
 
         // clean up
-        User::delete(id).expect("test clean-up fail");
+        User::delete(u0.id).expect("test clean-up fail");
     }
 
     #[test]
@@ -234,72 +274,93 @@ mod tests {
         database::init().expect("database initialization fail");
 
         let username = String::from("test_duplicate_insert");
-        let u0 = User {
-            id: Uuid::nil(),
-            username: username.clone(),
-            password: Sha256::digest("test_password_2").to_vec(),
-            login_at: None,
-            attempts: 0,
-        };
-        let id = u0.insert().expect("insert panic");
-        let u1 = User {
-            id: Uuid::nil(),
-            username: username.clone(),
-            password: Sha256::digest("test_password_3").to_vec(),
-            login_at: None,
-            attempts: 0,
-        };
+        let mut u0 = User::new(
+            username.clone(),
+            Sha256::digest("test_password_2").to_vec(),
+        );
+        u0.id = u0.insert().expect("insert panic");
+        let u1 = User::new(
+            username.clone(),
+            Sha256::digest("test_password_3").to_vec(),
+        );
         u1.insert().expect_err("duplicate insert");
 
         // clean up
-        User::delete(id).expect("test clean-up fail");
+        User::delete(u0.id).expect("test clean-up fail");
     }
 
     #[test]
     fn test_update() {
         database::init().expect("database initialization fail");
 
-        let mut u0 = User {
-            id: Uuid::nil(),
-            username: String::from("test_update"),
-            password: Sha256::digest("test_password_4").to_vec(),
-            login_at: None,
-            attempts: 0,
-        };
-        let id = u0.insert().expect("insert panic");
-        u0.id = id;
+        let mut u0 = User::new(
+            String::from("test_update"),
+            Sha256::digest("test_password_4").to_vec(),
+        );
+        u0.id = u0.insert().expect("insert panic");
 
         u0.username = String::from("test_update_2");
         u0.update().expect("update fail");
-        let u1 = User::by_id(id)
+        let u1 = User::by_id(u0.id)
             .expect("select panic")
             .expect("user does not exist");
         assert_eq!(u0.username, u1.username);
 
         u0.password = Sha256::digest("some_random_password").to_vec();
         u0.update().expect("update fail");
-        let u1 = User::by_id(id)
+        let u1 = User::by_id(u0.id)
             .expect("select panic")
             .expect("user does not exist");
         assert_eq!(u0.password, u1.password);
 
-        // should not be updated
-        u0.login_at = Some(Utc::now());
-        u0.update().expect("update fail");
-        let u1 = User::by_id(id)
-            .expect("select panic")
-            .expect("user does not exist");
-        assert_eq!(None, u1.login_at);
-
-        // should not be updated
-        u0.attempts = 10;
-        u0.update().expect("update fail");
-        let u1 = User::by_id(id)
-            .expect("select panic")
-            .expect("user does not exist");
-        assert_eq!(0, u1.attempts);
-
         // clean up
-        User::delete(id).expect("test clean-up fail");
+        User::delete(u0.id).expect("test clean-up fail");
+    }
+
+    #[test]
+    fn test_delete() {
+        database::init().expect("database initialization fail");
+
+        let mut u0 = User::new(
+            String::from("test_update"),
+            Sha256::digest("test_password_4").to_vec(),
+        );
+        u0.id = u0.insert().expect("insert panic");
+        User::delete(u0.id).expect("delete panic");
+
+        assert_eq!(None, User::by_id(u0.id).expect("select panic"));
+    }
+
+    #[test]
+    fn test_attempts() {
+        database::init().expect("database initialization fail");
+
+        let mut u0 = User::new(
+            String::from("test_attempts"),
+            Sha256::digest("test_password_4").to_vec(),
+        );
+        u0.id = u0.insert().expect("insert panic");
+
+        let a = u0.attempts().expect("cannot get attempts");
+        assert_eq!(0, a);
+
+        u0.add_attempt().expect("cannot add attempts");
+        let a = u0.attempts().expect("cannot get attempts");
+        assert_eq!(1, a);
+
+        u0.add_attempt().expect("cannot add attempts");
+        let a = u0.attempts().expect("cannot get attempts");
+        assert_eq!(2, a);
+
+        u0.add_attempt().expect("cannot add attempts");
+        let a = u0.attempts().expect("cannot get attempts");
+        assert_eq!(3, a);
+
+        thread::sleep(Duration::from_secs(70));
+
+        let a = u0.attempts().expect("cannot get attempts");
+        assert_eq!(0, a);
+
+        User::delete(u0.id).expect("test clean-up fail");
     }
 }
