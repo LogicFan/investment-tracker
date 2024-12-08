fbcc7b7fe2bac67814b2479a90cc7e3b79076aae
Logic <38597904+LogicFan@users.noreply.github.com>
2024-12-08 21:39:30 +0800

    add test

--- src/database/account/kind.rs
+++ src/database/account/kind.rs
@@ -10,33 +10,74 @@ pub enum AccountKind {
     FHSA,
 }
 
-impl From<AccountKind> for sea_query::value::Value {
+impl TryFrom<String> for AccountKind {
+    type Error = ();
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.as_str() {
+            "NRA" => Ok(Self::NRA),
+            "TFSA" => Ok(Self::TFSA),
+            "RRSP" => Ok(Self::RRSP),
+            "FHSA" => Ok(Self::FHSA),
+            _ => Err(()),
+        }
+    }
+}
+
+impl From<AccountKind> for String {
     fn from(value: AccountKind) -> Self {
         match value {
-            AccountKind::NRA => "NRA".into(),
-            AccountKind::TFSA => "TFSA".into(),
-            AccountKind::RRSP => "RRSP".into(),
-            AccountKind::FHSA => "FHSA".into(),
+            AccountKind::NRA => String::from("NRA"),
+            AccountKind::TFSA => String::from("TFSA"),
+            AccountKind::RRSP => String::from("RRSP"),
+            AccountKind::FHSA => String::from("FHSA"),
         }
+    }
+}
+
+impl From<AccountKind> for sea_query::value::Value {
+    fn from(value: AccountKind) -> Self {
+        String::from(value).into()
     }
 }
 
 impl FromSql for AccountKind {
     fn column_result(value: ValueRef<'_>) -> Result<Self, FromSqlError> {
-        if let ValueRef::Text(text) = value {
-            if let Ok(s) = str::from_utf8(text) {
-                match s {
-                    "NRA" => return Ok(Self::NRA),
-                    "TFSA" => return Ok(Self::TFSA),
-                    "RRSP" => return Ok(Self::RRSP),
-                    "FHSA" => return Ok(Self::FHSA),
-                    _ => (),
+        match value.as_str() {
+            Err(err) => Err(err),
+            Ok(str) => {
+                if let Ok(kind) = AccountKind::try_from(String::from(str)) {
+                    Ok(kind)
+                } else {
+                    Err(FromSqlError::Other(
+                        format!("{} is not a valid AccountKind", str).into(),
+                    ))
                 }
             }
         }
+    }
+}
 
-        Err(FromSqlError::Other(
-            String::from("Cannot convert to AccountKind").into(),
-        ))
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::error::ServerError;
+
+    #[test]
+    fn test_convert() -> Result<(), ServerError> {
+        fn assert_util(value: AccountKind) {
+            let value2 = AccountKind::try_from(String::from(value)).unwrap();
+            assert_eq!(value, value2);
+        }
+
+        assert_util(AccountKind::NRA);
+        assert_util(AccountKind::TFSA);
+        assert_util(AccountKind::RRSP);
+        assert_util(AccountKind::FHSA);
+
+        AccountKind::try_from(String::from("SOME RANDOM STRING"))
+            .expect_err("expect conversion failure");
+
+        Ok(())
     }
 }
