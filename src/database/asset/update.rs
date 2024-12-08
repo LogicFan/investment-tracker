b1cf6c1e9e0de904106a69f4762c844b51c82be5
Logic <38597904+LogicFan@users.noreply.github.com>
2024-12-08 22:45:34 +0800

    add update

--- src/database/asset/update.rs
+++ src/database/asset/update.rs
@@ -1,11 +1,12 @@
-use core::str;
+use crate::error::ServerError;
 use chrono::{DateTime, Utc};
+use core::str;
 use rusqlite::types::{FromSql, FromSqlError, ValueRef};
-use sea_query::enum_def;
+use rusqlite::Transaction as SqlTransaction;
+use sea_query::{enum_def, Expr, Query, SqliteQueryBuilder};
+use sea_query_rusqlite::RusqliteBinder;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
-
-use crate::error::ServerError;
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone, Copy)]
 pub enum AssetUpdateKind {
@@ -14,52 +15,141 @@ pub enum AssetUpdateKind {
     Split,
 }
 
-impl From<AssetUpdateKind> for sea_query::value::Value {
+impl TryFrom<String> for AssetUpdateKind {
+    type Error = ();
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.as_str() {
+            "Price" => Ok(Self::Price),
+            "Dividend" => Ok(Self::Dividend),
+            "Split" => Ok(Self::Split),
+            _ => Err(()),
+        }
+    }
+}
+
+impl From<AssetUpdateKind> for String {
     fn from(value: AssetUpdateKind) -> Self {
         match value {
-            AssetUpdateKind::Price => "Price".into(),
-            AssetUpdateKind::Dividend => "Dividend".into(),
-            AssetUpdateKind::Split => "Split".into(),
+            AssetUpdateKind::Dividend => String::from("Dividend"),
+            AssetUpdateKind::Price => String::from("Price"),
+            AssetUpdateKind::Split => String::from("Split"),
         }
+    }
+}
+
+impl From<AssetUpdateKind> for sea_query::value::Value {
+    fn from(value: AssetUpdateKind) -> Self {
+        String::from(value).into()
     }
 }
 
 impl FromSql for AssetUpdateKind {
     fn column_result(value: ValueRef<'_>) -> Result<Self, FromSqlError> {
-        if let ValueRef::Text(text) = value {
-            if let Ok(s) = str::from_utf8(text) {
-                match s {
-                    "Price" => return Ok(Self::Price),
-                    "Dividend" => return Ok(Self::Dividend),
-                    "Split" => return Ok(Self::Split),
-                    _ => (),
-                }
-            }
-        }
-
-        Err(FromSqlError::Other(
-            String::from("Cannot convert to AssetUpdateKind").into(),
-        ))
+        AssetUpdateKind::try_from(String::from(value.as_str()?)).map_err(|_| {
+            FromSqlError::Other(
+                String::from("Cannot convert to AssetUpdateKind").into(),
+            )
+        })
     }
 }
-
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[enum_def]
 pub(super) struct AssetUpdate {
-    #[serde(default)]
     pub asset: Uuid,
     pub query: AssetUpdateKind,
-    pub updated_at: DateTime<Utc>,
+    pub updated_at: (),
 }
 
-// impl AssetUpdate {
-//     pub(super) fn update(&self, transaction: &mut rusqlite::Transaction) -> Result<(), ServerError> {
+impl AssetUpdate {
+    pub(super) fn new(asset: Uuid, query: AssetUpdateKind) -> Self {
+        Self {
+            asset,
+            query,
+            updated_at: (),
+        }
+    }
 
-//     }
+    pub(super) fn get_update(
+        &self,
+        transaction: &SqlTransaction,
+    ) -> Result<DateTime<Utc>, ServerError> {
+        let (query, values) = Query::select()
+            .columns([AssetUpdateIden::UpdatedAt])
+            .from(AssetUpdateIden::Table)
+            .and_where(Expr::col(AssetUpdateIden::Asset).eq(self.asset))
+            .and_where(Expr::col(AssetUpdateIden::Query).eq(self.query))
+            .build_rusqlite(SqliteQueryBuilder);
 
-//     pub(super) fn update(&self, transaction: &mut rusqlite::Transaction) -> Result<(), ServerError> {
+        let mut statement = transaction.prepare(&query)?;
+        let record = statement
+            .query_and_then(&*values.as_params(), |row| row.get(0))?
+            .next();
 
-//     }
-// }
+        Ok(record.transpose()?.unwrap_or(DateTime::<Utc>::MIN_UTC))
+    }
 
+    pub(super) fn set_update(
+        &self,
+        updated_at: DateTime<Utc>,
+        transaction: &SqlTransaction,
+    ) -> Result<(), ServerError> {
+        let (query, values) = Query::insert()
+            .replace()
+            .into_table(AssetUpdateIden::Table)
+            .columns([
+                AssetUpdateIden::Asset,
+                AssetUpdateIden::Query,
+                AssetUpdateIden::UpdatedAt,
+            ])
+            .values([self.asset.into(), self.query.into(), updated_at.into()])?
+            .build_rusqlite(SqliteQueryBuilder);
+
+        transaction.execute(&query, &*values.as_params())?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::database;
+    use crate::database::asset::{Asset, AssetId};
+    use rusqlite::Connection;
+
+    #[test]
+    fn test_set_and_get() -> Result<(), ServerError> {
+        let mut conn = Connection::open_in_memory()?;
+
+        let a0 = {
+            let tran = conn.transaction()?;
+            database::migration::run_migration(&tran)?;
+            let mut a0 = Asset::new(
+                AssetId::currency("USD"),
+                "United States Dollar",
+                None,
+            );
+            a0.id = a0.insert(&tran)?;
+            tran.commit()?;
+            a0
+        };
+        let up = AssetUpdate::new(a0.id, AssetUpdateKind::Dividend);
+        {
+            let tran = conn.transaction()?;
+            let t = up.get_update(&tran)?;
+            assert_eq!(t, DateTime::<Utc>::MIN_UTC);
+        }
+        {
+            let tran = conn.transaction()?;
+            let t = Utc::now();
+            up.set_update(t, &tran)?;
+            tran.commit()?;
+            let tran = conn.transaction()?;
+            let t2 = up.get_update(&tran)?;
+            assert_eq!(t, t2);
+        }
+
+        Ok(())
+    }
+}
